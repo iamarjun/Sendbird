d85372736662405c3e3db1394cd376ac40498443
iamarjun <arjunmanoj1995@gmail.com>
2021-04-28 23:07:53 +0530

    Pass layout via constructor in the MainActivity.kt

--- app/src/main/java/com/arjun/sendbird/MainActivity.kt
+++ app/src/main/java/com/arjun/sendbird/MainActivity.kt
@@ -1,11 +1,7 @@
 package com.arjun.sendbird
 
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
 }
