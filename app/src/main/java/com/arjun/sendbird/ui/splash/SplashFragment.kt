64681518767b639d4f23cdee88ad8d3821b5df40
iamarjun <arjunmanoj1995@gmail.com>
2021-05-01 18:41:43 +0530

    Create SplashFragment.kt

---
+++ app/src/main/java/com/arjun/sendbird/ui/splash/SplashFragment.kt
@@ -0,0 +1,89 @@
+package com.arjun.sendbird.ui.splash
+
+import android.os.Bundle
+import android.view.View
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.ScaffoldState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import com.arjun.sendbird.MainViewModel
+import com.arjun.sendbird.R
+import com.arjun.sendbird.cache.UserManager
+import com.arjun.sendbird.ui.base.BaseFragment
+import com.google.accompanist.insets.ExperimentalAnimatedInsets
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import javax.inject.Inject
+
+@ExperimentalAnimationApi
+@ExperimentalAnimatedInsets
+@AndroidEntryPoint
+class SplashFragment : BaseFragment() {
+
+    private val viewModel by viewModels<MainViewModel>()
+
+    @Inject
+    internal lateinit var userManager: UserManager
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        userManager.getUserId().flowWithLifecycle(lifecycle, Lifecycle.State.STARTED)
+            .onEach {
+                if (it.isNotEmpty())
+                    viewModel.login(it)
+                else
+                    findNavController().navigate(SplashFragmentDirections.actionSplashFragmentToLoginFragment())
+            }.launchIn(lifecycleScope)
+
+        viewModel.userExist.flowWithLifecycle(lifecycle, Lifecycle.State.STARTED)
+            .onEach {
+                if (it) {
+                    findNavController().navigate(SplashFragmentDirections.actionSplashFragmentToChannelListFragment())
+                }
+
+            }.launchIn(lifecycleScope)
+    }
+
+    @Composable
+    override fun MainContent(paddingValues: PaddingValues, scaffoldState: ScaffoldState) {
+
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.logo_sendbird_full),
+                contentDescription = "Sendbird Logo",
+                alignment = Alignment.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 32.dp)
+            )
+
+            CircularProgressIndicator(
+                modifier = Modifier
+                    .size(64.dp)
+                    .padding(16.dp),
+            )
+        }
+    }
+
+    @Composable
+    override fun ToolBar() {
+
+    }
+}
