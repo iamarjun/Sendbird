51980e70d0be5aece75eb5ab98a4dd9bb5512712
iamarjun <arjunmanoj1995@gmail.com>
2021-05-01 18:43:13 +0530

    Annotate MainActivity.kt with @AndroidEntryPoint for dependency injection

--- app/src/main/java/com/arjun/sendbird/ui/MainActivity.kt
+++ app/src/main/java/com/arjun/sendbird/ui/MainActivity.kt
@@ -2,7 +2,9 @@ package com.arjun.sendbird.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import com.arjun.sendbird.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
 }
