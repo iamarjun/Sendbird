e4bbc3476c7d6b23a1fd5f9aa45743905ea4d5c2
iamarjun <arjun.manoj@practo.com>
2021-05-14 18:43:00 +0530

    Upgrade gradle and libs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -82,9 +82,9 @@ dependencies {
     implementation("com.google.dagger:hilt-android:2.35.1")
     kapt("com.google.dagger:hilt-compiler:2.35.1")
 
-    implementation("com.google.accompanist:accompanist-glide:0.9.0")
-    implementation("com.google.accompanist:accompanist-insets:0.9.0")
-    implementation("com.google.accompanist:accompanist-swiperefresh:0.9.0")
+    implementation("com.google.accompanist:accompanist-glide:0.9.1")
+    implementation("com.google.accompanist:accompanist-insets:0.9.1")
+    implementation("com.google.accompanist:accompanist-swiperefresh:0.9.1")
 
     implementation("com.sendbird.sdk:sendbird-android-sdk:3.0.163")
 
